87660f7a9f738b56012fc55f8be21c558e0f73d5
Sebastian Granda Gallego <sgg10.develop@gmail.com>
2022-08-21 16:17:10 -0500

    Added redis-cluster

--- projects/p01/progress01_redis/app/redis.py
+++ projects/p01/progress01_redis/app/redis.py
@@ -1,6 +1,6 @@
 from redis import StrictRedis
 
-def connect(host: str = "0.0.0.0", password: str = "abc1234567890", port: int = 6379, db: int = 0):
+def connect(host: str = "0.0.0.0", password: str = "abc123", port: int = 6379, db: int = 0):
     try:
         return StrictRedis(host=host, password=password, port=port, db=db)
     except:
